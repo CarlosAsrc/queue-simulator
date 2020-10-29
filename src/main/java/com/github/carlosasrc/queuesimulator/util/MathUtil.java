19ac9e3fa17c95d47735123d05f1b5e6c507f94b
CarlosAsrc <carlos.asrc@gmail.com>
2020-10-29 20:30:26 -0300

    Corrige simulaçao de rede de filas

--- src/main/java/com/github/carlosasrc/queuesimulator/util/MathUtil.java
+++ src/main/java/com/github/carlosasrc/queuesimulator/util/MathUtil.java
@@ -4,9 +4,6 @@ import com.github.carlosasrc.queuesimulator.model.report.SimpleQueueReport;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Arrays;
-import java.util.List;
-
 
 @Data
 @NoArgsConstructor
@@ -18,59 +15,15 @@ public class MathUtil {
     private double seed = 1;
     private int randomGenerated = 0;
 
-    private List<Double> aleatorios = Arrays.asList(0.2176,
-            0.0103,
-            0.1109,
-            0.3456,
-            0.9910,
-            0.2323,
-            0.9211,
-            0.0322,
-            0.1211
-//            0.5131,
-//            0.7208,
-//            0.9172,
-//            0.9922,
-//            0.8324,
-//            0.5011,
-//            0.2931,
-//            0.6621,
-//            0.8574,
-//            0.3964,
-//            0.4042,
-//            0.5058,
-//            0.8261,
-//            0.9902,
-//            0.1230,
-//            0.8496,
-//            0.2949,
-//            0.0839,
-//            0.3417,
-//            0.6933,
-//            0.2636,
-//            0.6777,
-//            0.0605,
-//            0.0371,
-//            0.7324,
-//            0.7714,
-//            0.2792,
-//            0.8808,
-//            0.7011,
-//            0.3652
-    );
-
 
     public double getNextRandomTime(Integer A, Integer B) {
         this.randomGenerated++;
         double bla = getRandomNumber();
-//        System.out.println(bla);
         return (B - A) * bla + A;
     }
 
     public double getRandomNumber() {
-//        return aleatorios.get(randomGenerated-1);
         seed = (A * seed + C) % M;
-//        System.out.println("ALEATORIO: " + seed / M);
         return seed / M;
     }
 
